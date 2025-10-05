d1a81aeb23efe1863f88eaf9102dc4ca9d4ca30b
わど <wado0891@gmail.com>
2025-10-05 21:12:27 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// ページフェードイン
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = 'opacity 1s';
+    document.body.style.opacity = 1;
+  }, 150);
+});
